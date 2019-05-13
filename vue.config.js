deb51c6e99013e772ba3d438dc4bf36bb1d4887a
ltadpole <ltadpole@163.com>
2019-05-13 11:31:56 +0800

    bulid

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,14 @@ module.exports = {
         port: 8080,
         host: 'localhost',
         open: true
+    },
+    chainWebpack(config) {
+        config.plugin('define').tap(args => {
+            args[0]['process.env'].VUE_APP_LOGOUT_URL = JSON.stringify(
+                process.env.VUE_APP_LOGOUT_URL
+            )
+            console.log(args[0])
+            return args
+        })
     }
 }
