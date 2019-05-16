0d502648ed0f6cd37515f15f508e6457d9c03262
ltadpole <ltadpole@163.com>
2019-05-16 15:17:39 +0800

    pro

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
         host: 'localhost',
         open: true
     },
+    /* eslint-disable */
     chainWebpack(config) {
         config.plugin('define').tap(args => {
             args[0]['process.env'].VUE_APP_LOGOUT_URL = JSON.stringify(
