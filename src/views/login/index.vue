3572e61eff1527dd9352463125ab062aea44920a
ltadpole <ltadpole@163.com>
2019-05-23 18:26:52 +0800

    footer

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="login">这是登录页</div>
+    <div>
+        <div class="login">这是登录页</div>
+        <div class="footer">
+            <app-footer></app-footer>
+        </div>
+    </div>
 </template>
 
 <script>
+import appFooter from '@/components/footer'
 export default {
     name: 'login',
     data() {
         return {
             test: 0
         }
+    },
+    components: {
+        appFooter
     }
 }
 </script>
