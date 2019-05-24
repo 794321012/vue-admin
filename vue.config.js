3014d835f331befca38594fb29417f278ca66dfa
ltadpole <ltadpole@163.com>
2019-05-24 16:30:42 +0800

    环境配置

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,19 @@
+const config = require('./config')
+
 module.exports = {
     devServer: {
         port: 8080,
         host: 'localhost',
-        open: true
-    },
-    /* eslint-disable */
-    chainWebpack(config) {
-        config.plugin('define').tap(args => {
-            args[0]['process.env'].VUE_APP_LOGOUT_URL = JSON.stringify(
-                process.env.VUE_APP_LOGOUT_URL
-            )
-            console.log(args[0])
-            return args
-        })
+        open: true,
+        proxy: {
+            [config.ROOT]: {
+                target: config.PROXYROOT,
+                changeOrigin: true,
+                pathRewrite: {
+                    [`^${config.ROOT}`]: ''
+                },
+                ws: false
+            }
+        }
     }
 }
